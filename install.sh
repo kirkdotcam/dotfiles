4c777a21ab2eb6845b69d305395cb0c42cd7388b
kirkdotcam <camden.kirkland@gmail.com>
2024-08-22 18:35:17 -0500

    add calls to language files and fix heredoc

--- install.sh
+++ install.sh
@@ -1,16 +1,32 @@
+. ./tools.sh
+. ./languages/rust.sh
+. ./languages/nodejs.sh
+. ./languages/python.sh
 
+. ./alias.sh
+
+# Don't run if bashrc is already configured
 if grep -q "#kirkdotcam config#" ~/.bashrc; then
 
   echo "kirkdotcam config found in .bashrc file. Exiting"
   exit 0
 fi
 
+
+
+# Modify bashrc
 cat ~/.bashrc > ~/.bashrc_old
 
 cat << EOF >> ~/.bashrc
 
 #kirkdotcam config#
-. ~/.config/dotfiles/alias.sh
+
+. ~/.config/dotfiles/alias.sh;
+
+export EDITOR="nvim";
+
+
+#kirkdotcam endconfig#
 EOF
 
 . ~/.bashrc
