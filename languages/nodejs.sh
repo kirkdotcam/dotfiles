d8b140e548b1928dc31b537391da3766c0859e8e
kirkdotcam <camden.kirkland@gmail.com>
2026-02-17 17:03:05 -0600

    update nodejs to always grab whatever is on master branch

--- languages/nodejs.sh
+++ languages/nodejs.sh
@@ -1,4 +1,4 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
